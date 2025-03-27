7e478dda8dee833d7c0a7cc659e36c495c8dd145
iceclimb-caver <robertlmartin23@gmail.com>
2025-03-27 07:57:40 -0400

    Update hello-world.go
    just changing this to python

--- hello-world.go
+++ hello-world.go
@@ -1,7 +1,8 @@
-package maint
-
-import "fmt"123
-
-func main() {deletethis
-    fmt.Println("hello world")
-}
+print("This line will be printed.")
+#change the above text to hello world
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+#the function above counts the number 1, change it to count the number 3
+print("Goodbye and thank you")
